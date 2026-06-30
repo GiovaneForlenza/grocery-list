0027b8bae062d1dc2975f9f534495ccc580ea9f8
Giovane Forlenza <giovane.franciscoforl@gmail.com>
2026-06-30 18:12:56 -0300

    Integrated Supabase and started initial tests

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,24 @@
+import supabase from "./supabase-client";
+
 function App() {
-  return <div className="flex border">App</div>;
+  const item = "Teste";
+
+  const handleClick = async () => {
+    const { data, error } = await supabase.from("Categories").select();
+
+    if (error) {
+      console.log(error);
+    } else {
+      console.log(data);
+    }
+  };
+
+  return (
+    <div className="flex border">
+      <h1>Grocery list</h1>
+      <button onClick={handleClick}>Add item</button>
+    </div>
+  );
 }
 
 export default App;
