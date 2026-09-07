9bcd94d0265baa02d10d410944353ad62a4bb5c5
Giovane Forlenza <giovane.franciscoforl@gmail.com>
2026-09-07 14:58:07 -0300

    Removed Why Is Node Running

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,8 @@
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
-import { createRequire } from "module";
 import { defineConfig } from "vite";
-import whyIsNodeRunning from "why-is-node-running";
-const require = createRequire(import.meta.url);
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-    {
-      name: "force-exit-after-build",
-      apply: "build",
-      closeBundle() {
-        whyIsNodeRunning();
-        setTimeout(() => process.exit(0), 0);
-      },
-    },
-  ],
+  plugins: [react(), tailwindcss()],
 });
